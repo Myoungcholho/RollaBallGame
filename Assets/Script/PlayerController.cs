9e842e1a93a8f899e20bb8904b9c5a04e57c179a
Myoungcholho <110521729+Myoungcholho@users.noreply.github.com>
2023-09-06 19:30:02 +0900

    명철호
    5까지

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -3,16 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+struct myStruct
+{
+    public string name;
+}
+
 public class PlayerController : MonoBehaviour
 {
+
+    public float speed;
+
+    private Rigidbody rb;
+    private myStruct myStruct;
+    private float movementX;
+    private float movementY;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+
+        if (rb == null)
+            Debug.Log("rb Null");
     }
 
     private void OnMove(InputValue movementValue)
     {
-        
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+    private void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        Debug.Log("X :" + movementX);
+        Debug.Log("Y :" + movementY);
+
+        rb.AddForce(movement * speed);
     }
 }
